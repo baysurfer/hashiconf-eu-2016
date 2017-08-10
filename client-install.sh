afafc209d44c5b792335451c67840539261a760b
Christian Madsen <madsen.chris@gmail.com>
2017-08-10 13:25:29 -0400

    Updated to Nomad 0.6.0

--- client-install.sh
+++ client-install.sh
@@ -6,14 +6,14 @@ export IP_ADDRESS=$(curl -s -H "Metadata-Flavor: Google" \
 apt-get update
 apt-get install -y unzip dnsmasq
 
-wget https://releases.hashicorp.com/nomad/0.5.6/nomad_0.5.6_linux_amd64.zip
-unzip nomad_0.5.6_linux_amd64.zip
+wget https://releases.hashicorp.com/nomad/0.6.0/nomad_0.6.0_linux_amd64.zip
+unzip nomad_0.6.0_linux_amd64.zip
 mv nomad /usr/local/bin/
 
 mkdir -p /var/lib/nomad
 mkdir -p /etc/nomad
 
-rm nomad_0.5.6_linux_amd64.zip
+rm nomad_0.6.0_linux_amd64.zip
 
 cat > client.hcl <<EOF
 addresses {
