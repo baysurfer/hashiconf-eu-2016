8d28a0f2a78326d5f5da5be14b356a9423dfb25a
Christian Madsen <madsen.chris@gmail.com>
2017-09-20 18:55:23 -0500

    Update Nomad to 0.6.3

--- server-install.sh
+++ server-install.sh
@@ -6,10 +6,10 @@ export IP_ADDRESS=$(curl -s -H "Metadata-Flavor: Google" \
 apt-get update
 apt-get install -y unzip dnsmasq
 
-wget https://releases.hashicorp.com/nomad/0.6.2/nomad_0.6.2_linux_amd64.zip
-unzip nomad_0.6.2_linux_amd64.zip
+wget https://releases.hashicorp.com/nomad/0.6.3/nomad_0.6.3_linux_amd64.zip
+unzip nomad_0.6.3_linux_amd64.zip
 mv nomad /usr/local/bin/nomad
-rm nomad_0.6.2_linux_amd64.zip
+rm nomad_0.6.3_linux_amd64.zip
 
 mkdir -p /var/lib/nomad
 mkdir -p /etc/nomad
