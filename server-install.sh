1e07f73d697540fc4e3046726a48247b124bb59e
Christian Madsen <madsen.chris@gmail.com>
2018-05-30 10:22:27 -0400

    Update Circonus, Nomad, Consul and Vault to latest

--- server-install.sh
+++ server-install.sh
@@ -6,10 +6,10 @@ export IP_ADDRESS=$(curl -s -H "Metadata-Flavor: Google" \
 apt-get update
 apt-get install -y unzip dnsmasq
 
-wget https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_linux_amd64.zip
-unzip nomad_0.7.1_linux_amd64.zip
+wget https://releases.hashicorp.com/nomad/0.8.3/nomad_0.8.3_linux_amd64.zip
+unzip nomad_0.8.3_linux_amd64.zip
 mv nomad /usr/local/bin/nomad
-rm nomad_0.7.1_linux_amd64.zip
+rm nomad_0.8.3_linux_amd64.zip
 
 mkdir -p /var/lib/nomad
 mkdir -p /etc/nomad
@@ -19,30 +19,25 @@ addresses {
     rpc  = "ADVERTISE_ADDR"
     serf = "ADVERTISE_ADDR"
 }
-
 advertise {
     http = "ADVERTISE_ADDR:4646"
     rpc  = "ADVERTISE_ADDR:4647"
     serf = "ADVERTISE_ADDR:4648"
 }
-
 bind_addr = "0.0.0.0"
 data_dir  = "/var/lib/nomad"
 log_level = "DEBUG"
-
 server {
     enabled = true
     bootstrap_expect = 3
 }
-
 telemetry {
 	circonus_api_token = "2c1518f9-10ae-49b8-9b04-c386616aae09"
-	circonus_check_tags = "source:gcp-cjm, type:server, service:hashistack, service:nomad"
+	circonus_check_tags = "source:gcp, type:server, service:hashistack, service:nomad"
      circonus_submission_interval = "1s"
      publish_node_metrics = "true"
      backwards_compatible_metrics = "true"
 }
-
 EOF
 sed -i "s/ADVERTISE_ADDR/${IP_ADDRESS}/" server.hcl
 mv server.hcl /etc/nomad/server.hcl
@@ -51,12 +46,10 @@ cat > nomad.service <<'EOF'
 [Unit]
 Description=Nomad
 Documentation=https://nomadproject.io/docs/
-
 [Service]
 ExecStart=/usr/local/bin/nomad agent -config /etc/nomad
 ExecReload=/bin/kill -HUP $MAINPID
 LimitNOFILE=65536
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -69,10 +62,10 @@ systemctl start nomad
 
 mkdir -p /var/lib/consul
 
-wget https://releases.hashicorp.com/consul/1.0.6/consul_1.0.6_linux_amd64.zip
-unzip consul_1.0.6_linux_amd64.zip
+wget https://releases.hashicorp.com/consul/1.1.0/consul_1.1.0_linux_amd64.zip
+unzip consul_1.1.0_linux_amd64.zip
 mv consul /usr/local/bin/consul
-rm consul_1.0.6_linux_amd64.zip
+rm consul_1.1.0_linux_amd64.zip
 
 mkdir -p /etc/consul
 
@@ -80,7 +73,7 @@ cat > /etc/consul/consul.json <<'EOF'
 {
 	"telemetry": {
           "circonus_api_token": "2c1518f9-10ae-49b8-9b04-c386616aae09",
-          "circonus_check_tags":  "source:gcp-cjm, type:server, service:consul, service:hashistack",
+          "circonus_check_tags":  "source:gcp, type:server, service:consul, service:hashistack",
           "circonus_submission_interval": "1s"
 	}
 }
@@ -90,7 +83,6 @@ cat > consul.service <<'EOF'
 [Unit]
 Description=consul
 Documentation=https://consul.io/docs/
-
 [Service]
 ExecStart=/usr/local/bin/consul agent \
   -advertise=ADVERTISE_ADDR \
@@ -101,10 +93,8 @@ ExecStart=/usr/local/bin/consul agent \
   -server \
   -ui \
   -config-file=/etc/consul/consul.json
-
 ExecReload=/bin/kill -HUP $MAINPID
 LimitNOFILE=65536
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -116,10 +106,10 @@ systemctl start consul
 
 ## Setup Vault
 
-wget https://releases.hashicorp.com/vault/0.9.5/vault_0.9.5_linux_amd64.zip
-unzip vault_0.9.5_linux_amd64.zip
+wget https://releases.hashicorp.com/vault/0.10.1/vault_0.10.1_linux_amd64.zip
+unzip vault_0.10.1_linux_amd64.zip
 mv vault /usr/local/bin/vault
-rm vault_0.9.5_linux_amd64.zip
+rm vault_0.10.1_linux_amd64.zip
 
 mkdir -p /etc/vault
 
@@ -129,18 +119,15 @@ backend "consul" {
   address = "127.0.0.1:8500"
   path = "vault"
 }
-
 listener "tcp" {
   address = "ADVERTISE_ADDR:8200"
   tls_disable = 1
 }
-
 telemetry {
 	circonus_api_token = "2c1518f9-10ae-49b8-9b04-c386616aae09"
-	circonus_check_tags = "source:gcp-cjm, type:server, service:hashistack, service:vault"
+	circonus_check_tags = "source:gcp, type:server, service:hashistack, service:vault"
      circonus_submission_interval = "1s"
 }
-
 EOF
 
 sed -i "s/ADVERTISE_ADDR/${IP_ADDRESS}/" /etc/vault/vault.hcl
@@ -149,14 +136,11 @@ cat > /etc/systemd/system/vault.service <<'EOF'
 [Unit]
 Description=Vault
 Documentation=https://vaultproject.io/docs/
-
 [Service]
 ExecStart=/usr/local/bin/vault server \
   -config /etc/vault/vault.hcl
-
 ExecReload=/bin/kill -HUP $MAINPID
 LimitNOFILE=65536
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -205,7 +189,7 @@ cat > /etc/consul/consul.json <<'EOF'
 {
 	"telemetry": {
           "circonus_api_token": "YOUR_CIRCONUS_TOKEN",
-          "circonus_check_tags":  "source:gcp-cjm, type:server, service:consul, service:hashistack",
+          "circonus_check_tags":  "source:gcp, type:server, service:consul, service:hashistack",
           "circonus_submission_interval": "1s"
 	}
 }
@@ -215,7 +199,6 @@ cat > consul.service <<'EOF'
 [Unit]
 Description=consul
 Documentation=https://consul.io/docs/
-
 [Service]
 ExecStart=/usr/local/bin/consul agent \
   -advertise=ADVERTISE_ADDR \
@@ -226,10 +209,8 @@ ExecStart=/usr/local/bin/consul agent \
   -server \
   -ui \
   -config-file=/etc/consul/consul.json
-
 ExecReload=/bin/kill -HUP $MAINPID
 LimitNOFILE=65536
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -254,18 +235,15 @@ backend "consul" {
   address = "127.0.0.1:8500"
   path = "vault"
 }
-
 listener "tcp" {
   address = "ADVERTISE_ADDR:8200"
   tls_disable = 1
 }
-
 telemetry {
 	circonus_api_token = "YOUR_CIRCONUS_TOKEN"
-	circonus_check_tags = "source:gcp-cjm, type:server, service:hashistack, service:vault"
+	circonus_check_tags = "source:gcp, type:server, service:hashistack, service:vault"
      	circonus_submission_interval = "1s"
 }
-
 EOF
 
 sed -i "s/ADVERTISE_ADDR/${IP_ADDRESS}/" /etc/vault/vault.hcl
@@ -274,14 +252,11 @@ cat > /etc/systemd/system/vault.service <<'EOF'
 [Unit]
 Description=Vault
 Documentation=https://vaultproject.io/docs/
-
 [Service]
 ExecStart=/usr/local/bin/vault server \
   -config /etc/vault/vault.hcl
-
 ExecReload=/bin/kill -HUP $MAINPID
 LimitNOFILE=65536
-
 [Install]
 WantedBy=multi-user.target
 EOF
